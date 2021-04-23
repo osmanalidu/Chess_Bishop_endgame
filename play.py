3e428d3eb94c1e44cefb8db5897db2323dec0de1
Raheel Y <ray37@cornell.edu>
2021-04-22 23:49:05 -0400

    set up 3 moves to checkmate endgame
    currently draws

--- play.py
+++ play.py
@@ -1,26 +1,43 @@
 import chess
 
+def setUpEndgame(f = "7k/8/3B1K2/5B2/8/8/8/8"):
+  #8 moves to mate ="6k1/8/4KBB1/8/8/8/8/8"
+  #3 moves to mate = "7k/8/3B1K2/5B2/8/8/8/8"
+  # - Kg6, kg8, Be6+, kh8, Be5#
+  return chess.Board(f)
+
+
 def run():
-  board = chess.Board()
+  board = setUpEndgame()
   
   
   while not board.is_game_over():
     #display board before turn
     board._repr_svg_()
+
+    print("-----------------------")
     print(board)
+    print("-----------------------")
 
     #move for current player
     legal_moves = list(board.legal_moves)
+    move = None
     if board.turn == chess.WHITE:
       #white move
-      board.push(legal_moves[0])
+      move = legal_moves[0]
     else:
-      board.push(legal_moves[0])
+      move = legal_moves[0]
+    
+    board.push(move)
+    # print(move)
 
+  print("\n\nFinal Position:\n", board)
   if board.is_checkmate():
     if board.turn == chess.WHITE:
-      print("White Wins")
-    else:
       print("Black Wins")
+    else:
+      print("White Wins")
   else:
     print("Draw")
+
+run()
