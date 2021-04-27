acfe5896abac1b8e37f3fbf95655d68c10cbd8bb
osmanalidu <alidunapare00@gmail.com>
2021-04-27 15:54:04 -0400

    grahics working

---
+++ boardgrahpics.py
@@ -0,0 +1,61 @@
+from graphics import *
+
+windowHeight = 650
+windowWidth = 650
+imgDict = {"B": "wbishop.png", "K": "wking.png",
+           "k": "bking.png", "B1": "wbishop.png"}
+
+
+class BoardGraphics():
+    def __init__(self):
+        self.window = GraphWin("Chess Board", windowWidth, windowHeight)
+        self.boxes = []
+        self.texts = []
+
+        for row in range(8):
+            gridRow = []
+            p1y = (75 * row) + 25
+            p2y = p1y + 75
+            for col in range(8):
+                p1x = (75 * col) + 25
+                p2x = p1x + 75
+                curr = Rectangle(Point(p1x, p1y), Point(p2x, p2y))
+                if (row % 2) != (col % 2):
+                    curr.setFill("green")
+                gridRow.append(curr)
+            self.boxes.append(gridRow)
+
+    # given a dictionary of positions for all the pieces in a game
+    # draw a pretty chess board with the pieces at those locations
+    def updateBoard(self, positions):
+        for t in self.texts:
+            t.undraw()
+        self.texts = []
+        for pieceName in list(positions.keys()):
+            print(positions.keys())
+            p = positions[pieceName]
+            c = self.boxes[p[0]][p[1]].getCenter()
+
+            # comment out the lines with i, comment in those with t
+            # if images are not working with your system
+            # t = Text(c, pieceName)
+            # t.draw(self.window)
+            # self.texts.append(t)
+            i = Image(c, imgDict[pieceName])
+            i.draw(self.window)
+            self.texts.append(i)
+
+    # draws the chessboard (background, not the pieces)
+    def drawGrid(self):
+        for row in self.boxes:
+            for item in row:
+                item.draw(self.window)
+
+
+# def main():
+#     b = BoardGraphics()
+#     b.drawGrid()
+
+
+# if __name__ == "__main__":
+#     main()
--- play.py
+++ play.py
@@ -1,56 +1,94 @@
+import sys
 import chess
+import time
+from boardgrahpics import *
 
-def set_up_endgame(f = "7k/8/3B1K2/5B2/8/8/8/8"):
-  #8 moves to mate ="6k1/8/4KBB1/8/8/8/8/8"
-  #3 moves to mate = "7k/8/3B1K2/5B2/8/8/8/8"
-  # - Kg6, kg8, Be6+, kh8, Be5#
-  return chess.Board(f)
+
+def set_up_endgame(f="6k1/8/4KBB1/8/8/8/8/8"):
+    # 8 moves to mate ="6k1/8/4KBB1/8/8/8/8/8"
+    # 3 moves to mate = "7k/8/3B1K2/5B2/8/8/8/8"
+    # - Kg6, kg8, Be6+, kh8, Be5#
+    return chess.Board(f)
+
 
 def game_over_message(board):
-  print("\n\nFinal Position:\n", board)
-  if board.is_checkmate():
-    if board.turn == chess.WHITE:
-      print("Black Wins")
+    print("\n\nFinal Position:\n", board)
+    if board.is_checkmate():
+        if board.turn == chess.WHITE:
+            print("Black Wins")
+        else:
+            print("White Wins")
     else:
-      print("White Wins")
-  else:
-    print("Draw")
+        print("Draw")
+
 
 def human_move(legal_moves):
-  #print list of legal moves
-  for move in legal_moves:
-    print(move)
+    # print list of legal moves
+    for move in legal_moves:
+        print(move)
 
-  #get user move as input / 
-  #TODO: check if legal
-  u_move = input("Choose a move from the list")
-  move = chess.Move.from_uci(u_move)
-  return move
+    # get user move as input /
+    # TODO: check if legal
+    u_move = input("Choose a move from the list")
+    move = chess.Move.from_uci(u_move)
+    return move
+
+
+def getFen(board):
+    return board.epd().split()[0]
+
+
+def getPiecePositions(board):
+    currFen = getFen(board)
+    pieces = ["B", "K", "k"]
+    lines = currFen.split("/")
+    piecePlaces = {}
+    for i in range(len(lines)):
+        j = 0
+        for char in lines[i]:
+            if char not in pieces:
+                j += int(char)
+            else:
+                if char in piecePlaces:
+                    piecePlaces[char+"1"] = (i, j)
+                else:
+                    piecePlaces[char] = (i, j)
+                j += 1
+    return piecePlaces
+
 
 def run():
-  board = set_up_endgame()
-  
-  while not board.is_game_over():
-    #display board before turn
-    board._repr_svg_()
+    board = set_up_endgame()
+    VisualBoard = BoardGraphics()
+    VisualBoard.drawGrid()
 
-    print("-----------------------")
-    print(board)
-    print("-----------------------")
+    while not board.is_game_over():
+        # display board before turn
+        board._repr_svg_()
+        VisualBoard.updateBoard(getPiecePositions(board))
 
-    #move for current player
-    legal_moves = list(board.legal_moves)
-    move = None
-    if board.turn == chess.WHITE:
-      #white move
-      move = legal_moves[0]
-    else:
-      move = human_move(legal_moves)
-    
-    board.push(move)
-    # print(move)
+        print("-----------------------")
+        print(board)
+        print("-----------------------")
 
-  game_over_message(board)
-  
+        # move for current player
+        legal_moves = list(board.legal_moves)
+        move = None
+        if board.turn == chess.WHITE:
+            # white move
+            move = legal_moves[0]
+        else:
+            move = human_move(legal_moves)
 
-run()
+        board.push(move)
+        # print(move)
+
+    game_over_message(board)
+
+
+def main():
+    run()
+
+
+if __name__ == "__main__":
+    main()
